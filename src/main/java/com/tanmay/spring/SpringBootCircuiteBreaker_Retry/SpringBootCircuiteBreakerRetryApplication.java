af184548c14203fe06aaf8722de9951176529f0b
t@nm@y <writetotkpatra@gmail.com>
2019-02-02 22:50:22 +0530

    Initial Commit

---
+++ src/main/java/com/tanmay/spring/SpringBootCircuiteBreaker_Retry/SpringBootCircuiteBreakerRetryApplication.java
@@ -0,0 +1,14 @@
+package com.tanmay.spring.SpringBootCircuiteBreaker_Retry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCircuiteBreakerRetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCircuiteBreakerRetryApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tanmay/spring/SpringBootCircuiteBreaker_Retry/SpringBootCircuiteBreakerRetryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tanmay.spring.SpringBootCircuiteBreaker_Retry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootCircuiteBreakerRetryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
