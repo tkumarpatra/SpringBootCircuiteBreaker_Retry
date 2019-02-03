5262d00a446253ce921c86f2e3d22dac846000a9
t@nm@y <writetotkpatra@gmail.com>
2019-02-03 07:27:36 +0530

    Working @Retry

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # SpringBootCircuiteBreaker_Retry
 With this code I would try to implement circuit breaker followed by Retry and then combination of both.
 
-Endpoint URL: http://localhost:8080/iamendpoint
 Hystrix URL:  http://localhost:8080/hystrix
+
+Endpoint to test Hystrix URL:       http://localhost:8080/endpointToTestHystrix
+Endpoint to test Retry URL:         http://localhost:8080/endpointToTestRetry
+
+
--- src/main/java/com/tanmay/spring/SpringBootCircuiteBreaker_Retry/SpringBootCircuiteBreakerRetryApplication.java
+++ src/main/java/com/tanmay/spring/SpringBootCircuiteBreaker_Retry/SpringBootCircuiteBreakerRetryApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.retry.annotation.CircuitBreaker;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableRetry
 @EnableCircuitBreaker
 @EnableHystrixDashboard
 @SpringBootApplication
@@ -21,31 +26,74 @@ public class SpringBootCircuiteBreakerRetryApplication {
 	@RestController
 	public class RestEndPoint {
 
-		private final ServiceThatFails serviceThatFails;
-		public RestEndPoint(ServiceThatFails serviceThatFails){
-			this.serviceThatFails = serviceThatFails;
+		private final ServiceThatFails_Hystrix serviceThatFails_Hystrix;
+		private final ServiceThatFails_Retry serviceThatFails_retry;
+
+		public RestEndPoint(
+		        ServiceThatFails_Hystrix serviceThatFails_Hystrix,
+                            ServiceThatFails_Retry serviceThatFails_retry){
+			this.serviceThatFails_Hystrix = serviceThatFails_Hystrix;
+			this.serviceThatFails_retry = serviceThatFails_retry;
 		}
 
-		@GetMapping(value = "/iamendpoint")
-		public int callEndPoint() throws Exception {
-			return serviceThatFails.callBackEndServiceWhichFails();
+		@GetMapping(value = "/endpointToTestHystrix")
+		public int callEndPoint1() throws Exception {
+			return serviceThatFails_Hystrix.callBackEndServiceWhichFails_hystrix();
 		}
+
+        @GetMapping(value = "/endpointToTestRetry")
+        public int callEndPoint2() throws Exception {
+            return serviceThatFails_retry.callBackEndServiceWhichFails_HystrixWithRetry();
+        }
 	}
 
+	class BackEndFailedException extends RuntimeException{
+        public BackEndFailedException(String message) {
+            super(message);
+        }
+    }
+
 	@Service
-	class ServiceThatFails{
+	class ServiceThatFails_Hystrix {
 
 		public int callFallbackMethod(){
 			return 2;
 		}
 
 		@HystrixCommand(fallbackMethod = "callFallbackMethod")
-		public int callBackEndServiceWhichFails() throws Exception {
+		public int callBackEndServiceWhichFails_hystrix() throws Exception {
 			if(Math.random() > .5){
 				Thread.sleep(1000);
-				throw new RuntimeException();
+                throw new BackEndFailedException("Back End Failed");
 			}
 			return 1;
 		}
 	}
+
+    @Service
+    class ServiceThatFails_Retry {
+	    int retryCount = 0;
+
+	    @Recover
+        public int callFallbackMethod(BackEndFailedException ex){
+	        retryCount = 0;
+	        return 2;
+        }
+
+        @Retryable(include = BackEndFailedException.class,
+                        maxAttempts = 4)
+        public int callBackEndServiceWhichFails_HystrixWithRetry() throws Exception {
+
+	        if(retryCount == 0)
+	            retryCount ++;
+	        if(retryCount == 1)
+                System.out.println("First Retry");retryCount ++;
+            if(retryCount == 2)
+                System.out.println("Second Retry");retryCount ++;
+            if(retryCount == 3)
+                System.out.println("Third Retry");retryCount ++;
+
+	        throw new BackEndFailedException("Back End Failed");
+        }
+    }
 }
