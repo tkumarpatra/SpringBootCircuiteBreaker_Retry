5df92bc94933f87b061a0dc7fbc0dc34e0d6efe9
t@nm@y <writetotkpatra@gmail.com>
2019-02-03 05:50:45 +0530

    Working Hystrix Dashboard

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SpringBootCircuiteBreaker_Retry
 With this code I would try to implement circuit breaker followed by Retry and then combination of both.
+
+Endpoint URL: http://localhost:8080/iamendpoint
+Hystrix URL:  http://localhost:8080/hystrix
--- src/main/java/com/tanmay/spring/SpringBootCircuiteBreaker_Retry/SpringBootCircuiteBreakerRetryApplication.java
+++ src/main/java/com/tanmay/spring/SpringBootCircuiteBreaker_Retry/SpringBootCircuiteBreakerRetryApplication.java
@@ -1,8 +1,16 @@
 package com.tanmay.spring.SpringBootCircuiteBreaker_Retry;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @SpringBootApplication
 public class SpringBootCircuiteBreakerRetryApplication {
 
@@ -10,5 +18,34 @@ public class SpringBootCircuiteBreakerRetryApplication {
 		SpringApplication.run(SpringBootCircuiteBreakerRetryApplication.class, args);
 	}
 
-}
+	@RestController
+	public class RestEndPoint {
 
+		private final ServiceThatFails serviceThatFails;
+		public RestEndPoint(ServiceThatFails serviceThatFails){
+			this.serviceThatFails = serviceThatFails;
+		}
+
+		@GetMapping(value = "/iamendpoint")
+		public int callEndPoint() throws Exception {
+			return serviceThatFails.callBackEndServiceWhichFails();
+		}
+	}
+
+	@Service
+	class ServiceThatFails{
+
+		public int callFallbackMethod(){
+			return 2;
+		}
+
+		@HystrixCommand(fallbackMethod = "callFallbackMethod")
+		public int callBackEndServiceWhichFails() throws Exception {
+			if(Math.random() > .5){
+				Thread.sleep(1000);
+				throw new RuntimeException();
+			}
+			return 1;
+		}
+	}
+}
